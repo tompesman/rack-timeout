e8d9e527476ca1415df84b16b78d242a2a44a7de
Tom Pesman <tom@tnux.net>
2013-03-09 14:22:26 +0100

    added logging to stderr

--- lib/rack/timeout.rb
+++ lib/rack/timeout.rb
@@ -1,5 +1,5 @@
-require RUBY_VERSION < '1.9' && RUBY_PLATFORM != "java" ? 'system_timer' : 'timeout'
-SystemTimer ||= Timeout
+require RUBY_VERSION < '1.9' && RUBY_PLATFORM != 'java' ? 'system_timer' : 'timeout'
+Timeout ||= SystemTimer
 
 module Rack
   class Timeout
@@ -13,9 +13,22 @@ module Rack
     end
 
     def call(env)
-      status, headers, response = SystemTimer.timeout(self.class.timeout, ::Timeout::Error) { @app.call(env) }
-      Rails.logger.info "Timeout::Error env: #{env}" if status == 500
-      [status, headers, response]
+      t0, t, path = Time.now.utc, self.class.timeout, env['REQUEST_PATH']
+      begin
+        log "about to start handling request for %s with a timeout of %d seconds.", path, t
+        retval = ::Timeout.timeout(self.class.timeout, ::Timeout::Error) { @app.call(env) }
+        t1 = Time.now.utc
+      rescue ::Timeout::Error
+        log "request for %s aborted after a timeout of %d seconds.", path, t
+        log "request env: %s", env.inspect
+        raise
+      end
+      log "request for %s completed in about %0.2f seconds.", path, (t1 - t0)
+      retval
+    end
+
+    def log(s, *vs)
+      $stderr.puts "rack-timeout: #{s}" % vs
     end
 
   end
